1d51f9113ffb490ad20cdf3d33c6a6ee7031a9ea
Tim Hanewich <tahanewich@live.com>
2021-04-26 21:26:09 -0400

    Added buffer at end of welcome

--- home/resize.js
+++ home/resize.js
@@ -3,6 +3,7 @@ function ResizeBg() {
     //Do whichever is higher: the window height or the content height
     var wHeight = window.innerHeight;
     var cHeight = document.getElementById("opening").scrollHeight;
+    cHeight = cHeight + 100; //Add 100 so it isnt so squished in there if we need to expand to the height of the content
     var toSetTo = Math.max(wHeight, cHeight);
     document.getElementById("welcome").setAttribute("style", "height: " + toSetTo.toString() + "px");
 }
--- home/resize.ts
+++ home/resize.ts
@@ -3,6 +3,7 @@ function ResizeBg()
     //Do whichever is higher: the window height or the content height
     let wHeight:number = window.innerHeight;
     let cHeight:number = (document.getElementById("opening") as HTMLElement).scrollHeight;
+    cHeight = cHeight + 100; //Add 100 so it isnt so squished in there if we need to expand to the height of the content
     let toSetTo:number = Math.max(wHeight, cHeight);
     (document.getElementById("welcome") as HTMLElement).setAttribute("style", "height: " + toSetTo.toString() + "px");
 }
