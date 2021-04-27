153ecf34b2cea00c229c6af203b974c494130dc5
Tim Hanewich <tahanewich@live.com>
2021-04-26 21:24:57 -0400

    Fixed the html tag overlap issue

--- home/resize.js
+++ home/resize.js
@@ -2,10 +2,9 @@
 function ResizeBg() {
     //Do whichever is higher: the window height or the content height
     var wHeight = window.innerHeight;
-    var cHeight = document.getElementById("welcome").offsetHeight;
+    var cHeight = document.getElementById("opening").scrollHeight;
     var toSetTo = Math.max(wHeight, cHeight);
     document.getElementById("welcome").setAttribute("style", "height: " + toSetTo.toString() + "px");
-    console.log("Set to " + toSetTo.toString() + "px");
 }
 window.onresize = ResizeBg;
 ResizeBg();
--- home/resize.ts
+++ home/resize.ts
@@ -2,11 +2,9 @@ function ResizeBg()
 {
     //Do whichever is higher: the window height or the content height
     let wHeight:number = window.innerHeight;
-    let cHeight:number = (document.getElementById("welcome") as HTMLElement).offsetHeight;
+    let cHeight:number = (document.getElementById("opening") as HTMLElement).scrollHeight;
     let toSetTo:number = Math.max(wHeight, cHeight);
     (document.getElementById("welcome") as HTMLElement).setAttribute("style", "height: " + toSetTo.toString() + "px");
-
-    console.log("Set to " + toSetTo.toString() + "px");
 }
 
 window.onresize = ResizeBg;
