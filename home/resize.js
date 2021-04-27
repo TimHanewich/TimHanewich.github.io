b03d6f26c99e98bb5f2da945579f33884f639e89
Tim Hanewich <tahanewich@live.com>
2021-04-26 21:07:24 -0400

    Rewrote resize

--- home/resize.js
+++ home/resize.js
@@ -1,6 +1,11 @@
 "use strict";
 function ResizeBg() {
-    document.getElementById("welcome").setAttribute("style", "height: " + window.innerHeight.toString() + "px");
+    //Do whichever is higher: the window height or the content height
+    var wHeight = window.innerHeight;
+    var cHeight = document.getElementById("welcome").offsetHeight;
+    var toSetTo = Math.max(wHeight, cHeight);
+    document.getElementById("welcome").setAttribute("style", "height: " + toSetTo.toString() + "px");
+    console.log("Set to " + toSetTo.toString() + "px");
 }
 window.onresize = ResizeBg;
 ResizeBg();
--- home/resize.ts
+++ home/resize.ts
@@ -1,6 +1,12 @@
 function ResizeBg()
 {
-    (document.getElementById("welcome") as HTMLElement).setAttribute("style", "height: " + window.innerHeight.toString() + "px");
+    //Do whichever is higher: the window height or the content height
+    let wHeight:number = window.innerHeight;
+    let cHeight:number = (document.getElementById("welcome") as HTMLElement).offsetHeight;
+    let toSetTo:number = Math.max(wHeight, cHeight);
+    (document.getElementById("welcome") as HTMLElement).setAttribute("style", "height: " + toSetTo.toString() + "px");
+
+    console.log("Set to " + toSetTo.toString() + "px");
 }
 
 window.onresize = ResizeBg;
